82205f31e76282623e06bbe825d2977257621689
Shiva Singh <shivasingh.200087@gmail.com>
2022-11-29 01:01:32 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+import './bootstrap/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hiiii
-    </div>
+    <Routes>
+      <Route path="*" element={<div className='display-5 font-bolder text-center text-e9-primary'>404</div>} />
+    </Routes>
   );
 }
 
